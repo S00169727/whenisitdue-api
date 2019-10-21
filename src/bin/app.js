796e97e0e23ec43ff87b9f9f5027169f6d8d8e16
hignz <higz-14@live.com>
2019-10-21 02:12:43 +0100

    added: inital app.js setup

---
+++ src/bin/app.js
@@ -0,0 +1,83 @@
+const dotenv = require('dotenv');
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const morgan = require('morgan');
+const chalk = require('chalk');
+const helmet = require('helmet');
+
+/**
+ * Environment Variables
+ */
+dotenv.config();
+
+/**
+ * Express App
+ */
+const app = express();
+const PORT = process.env.PORT || 1111;
+
+/**
+ * Middleware
+ */
+console.log(chalk.blue('Initializing Middleware...'));
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: false,
+  }),
+);
+app.use(morgan('dev'));
+
+// app.use('/', require('../routes/users'));
+// app.use('/', require('../routes/skills'));
+// app.use('/', require('../routes/activities'));
+
+/**
+ * Error handling
+ */
+app.use((req, res, next) => {
+  const error = new Error('Route not found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res) => {
+  res.status(error.status || 500).json({
+    message: error.message,
+  });
+});
+
+/**
+ * MongoDB Connection
+ */
+console.log(chalk.blue('Establishing Database Connection...'));
+mongoose
+  .connect(process.env.MongoURI,
+    { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+  .then(() => {
+    app.listen(PORT, console.log(chalk.yellow(`✔︎ Server started on port ${PORT}`)));
+  })
+  .catch((err) => {
+    console.log(err);
+    console.log(chalk.red('Shutting down OSKills Server'));
+  });
+
+/**
+ * Database Connection Events
+ */
+const { connection } = mongoose;
+connection.on('connected', () => {
+  console.log(chalk.green(`✔︎ Connected to Database: ${process.env.MongoURI}`));
+});
+connection.on('error', (err) => {
+  console.log(chalk.red(`✘ Database Error: ${err}`));
+});
+connection.on('disconnected', () => {
+  console.log(chalk.red('✘ Disconnected from Database'));
+});
+connection.on('reconnected', () => {
+  console.log(chalk.green(`✔︎ Reconnected to Database: ${process.env.MongoURI}`));
+});
