d4b902510c76983a370b4e08a1d2d729cee8b127
Conor Fearon <S00169727@mail.itsligo.ie>
2020-01-10 18:59:52 +0000

    Removed unnecessary id Added referemce to the post model

--- src/models/Comment.js
+++ src/models/Comment.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const CommentSchema = new mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
   message: {
     type: String,
     required: true,
@@ -9,6 +8,10 @@ const CommentSchema = new mongoose.Schema({
   createdAt: {
     type: Date,
     default: Date.now,
+  },
+  post: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Post',
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
